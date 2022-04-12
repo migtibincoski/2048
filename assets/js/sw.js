a134c472140ef4172d50b201b6458e6e8198066d
tibincoskimiguel@gmail.com <tibincoskimiguel@gmail.com>
2022-04-12 14:21:38 -0300

    Correção de Bug #02

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,7 +1,7 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
 // List the files to precache
-const precacheResources = ['/',
+const filesToCache = ['/',
 '/index.html',
 '/assets/css/style.css',
 '/assets/js/game_manager.js',
@@ -17,24 +17,17 @@ const precacheResources = ['/',
 '/assets/js/keyboard_input_manager.js'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
-self.addEventListener('install', (event) => {
-  console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
-
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activate event!');
-});
-
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    }),
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
